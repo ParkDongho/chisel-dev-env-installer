ca1cda4a88bd41eebc4daca0ec357b74d5ba3552
ParkDongho <parkdongho28@naver.com>
2023-04-05 14:25:53 +0900

    Add: gui-rhel-7.sh

--- code/default/gui-rhel-8.sh
+++ code/default/gui-rhel-8.sh
@@ -2,7 +2,4 @@ sudo yum -y group list
 sudo yum -y groupinstall "Server with GUI"
 sudo systemctl set-default graphical
 
-sudo systemctl start xrdp
-sudo systemctl enable xrdp
-sudo systemctl disable firewalld
-sudo systemctl stop firewalld
+
--- code/default/xrdp.sh
+++ code/default/xrdp.sh
@@ -1,3 +1,6 @@
+sudo yum -y install xorgxrdp
+sudo yum -y install xrdp tigervnc-server
+
 firewall-cmd --add-service=xrdp --permanent
 firewall-cmd --permanent --add-port=3389/tcp
 firewall-cmd --reload
