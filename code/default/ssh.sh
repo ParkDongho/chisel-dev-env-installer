4c5dc9be053ed2ec76a4bc3c23ba30a71575f368
Dongho Park <parkdongho28@naver.com>
2023-04-07 02:21:33 +0900

    Update ssh.sh

--- code/default/ssh.sh
+++ code/default/ssh.sh
@@ -1,3 +1,3 @@
-firewall-cmd --add-service=ssh --permanent
-firewall-cmd --permanent --zone=public --add-port=22/tcp
-firewall-cmd --reload
+sudo firewall-cmd --add-service=ssh --permanent
+sudo firewall-cmd --permanent --zone=public --add-port=22/tcp
+sudo firewall-cmd --reload
