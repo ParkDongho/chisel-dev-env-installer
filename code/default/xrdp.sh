ab8d9c1d83809efbffda486aa2e4e67d809c3cb0
Dongho Park <parkdongho28@naver.com>
2023-04-07 02:21:53 +0900

    Update xrdp.sh

--- code/default/xrdp.sh
+++ code/default/xrdp.sh
@@ -1,6 +1,6 @@
 sudo yum -y install xorgxrdp
 sudo yum -y install xrdp tigervnc-server
 
-firewall-cmd --add-service=xrdp --permanent
-firewall-cmd --permanent --add-port=3389/tcp
-firewall-cmd --reload
+sudo firewall-cmd --add-service=xrdp --permanent
+sudo firewall-cmd --permanent --add-port=3389/tcp
+sudo firewall-cmd --reload
