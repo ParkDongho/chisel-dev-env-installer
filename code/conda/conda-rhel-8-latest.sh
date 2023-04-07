3c91f7aa6fc37d629fe523540be42f3e3bd4690e
Dongho Park <parkdongho28@naver.com>
2023-04-07 22:02:05 +0900

    Update conda-rhel-8-latest.sh

--- code/conda/conda-rhel-8-latest.sh
+++ code/conda/conda-rhel-8-latest.sh
@@ -5,8 +5,9 @@ echo "RUN : conda-rhel-8-latest.sh"
 echo "============================"
 
 # Conda
-wget "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
-bash Mambaforge-$(uname)-$(uname -m).sh
+# wget "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
+wget https://github.com/conda-forge/miniforge/releases/download/4.12.0-2/Mambaforge-Linux-x86_64.sh
+bash Mambaforge-Linux-x86_64.sh
 rm Mambaforge-Linux-x86_64.sh
 
 conda config --set auto_activate_base false
