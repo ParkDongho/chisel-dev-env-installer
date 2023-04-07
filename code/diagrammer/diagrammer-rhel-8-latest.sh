ef7e03a32b1024644e5262b7b84311b25fa8562b
Dongho Park <parkdongho28@naver.com>
2023-04-07 20:02:36 +0900

    Update diagrammer-rhel-8-latest.sh

--- code/diagrammer/diagrammer-rhel-8-latest.sh
+++ code/diagrammer/diagrammer-rhel-8-latest.sh
@@ -1,5 +1,2 @@
 sudo yum -y install graphviz
-cd ~
-mkdir -f .diagrammer
-cd .diagrammer
-git clone https://github.com/freechipsproject/diagrammer
+git clone https://github.com/freechipsproject/diagrammer ~/.diagrammer
