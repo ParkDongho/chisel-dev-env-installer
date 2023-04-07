c0cb298d98d567014d68bcecf8e791daf57b204e
Dongho Park <parkdongho28@naver.com>
2023-04-07 14:11:07 +0900

    Update and rename ohmyzsh-rhel-8-latest.sh to zsh-rhel-8-latest.sh

--- code/ohmyzsh/zsh-rhel-8-latest.sh
+++ code/ohmyzsh/zsh-rhel-8-latest.sh
@@ -1,7 +1,4 @@
 sudo yum -y install zsh && \
 sudo yum -y install util-linux-user && \
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
-chsh -s /bin/zsh && \
-cp -f ./.zshrc ~/.zshrc && \
-cd ~ && \
-source .zshrc
+chsh -s /bin/zsh
