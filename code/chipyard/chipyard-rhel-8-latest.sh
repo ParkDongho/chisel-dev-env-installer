b1bcc2c6a20b1ba592d2deb4372268f9539aaa10
Dongho Park <parkdongho28@naver.com>
2023-04-07 19:54:48 +0900

    Update chipyard-rhel-8-latest.sh

--- code/chipyard/chipyard-rhel-8-latest.sh
+++ code/chipyard/chipyard-rhel-8-latest.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
+echo "==============================="
+echo "RUN : chipyard-rhel-8-latest.sh"
+echo "==============================="
+
+# Conda
+conda activate base
+conda install -n base conda-lock
+conda activate base
+
 # Chipyard
-cd
-mkdir dev 
-cd ~/dev
+mkdir ~/dev && cd ~/dev
 git clone https://github.com/ucb-bar/chipyard.git
 cd ~/dev/chipyard
 git checkout 1.9.0
 ./build-setup.sh riscv-tools
 
+echo "================================"
+echo "EXIT : chipyard-rhel-8-latest.sh"
+echo "================================"
