b938647e9c07ce0dc25fd6d8a3af97b5290e59db
Dongho Park <parkdongho28@naver.com>
2023-04-07 19:45:42 +0900

    Update conda-rhel-8-latest.sh

--- code/conda/conda-rhel-8-latest.sh
+++ code/conda/conda-rhel-8-latest.sh
@@ -7,15 +7,12 @@ echo "============================"
 # Conda
 wget "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
 bash Mambaforge-$(uname)-$(uname -m).sh
-
 echo "Install Complete : Conda"
 
 source ~/.zshrc
-
 echo "Sourcing : .zshrc"
 
 conda config --set auto_activate_base false
-
 echo "Setup : Config"
 
 echo "============================"
