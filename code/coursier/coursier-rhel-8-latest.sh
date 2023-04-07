0a2593c4f3a23ea6a77adb57ad51199cd2b0b5a3
Dongho Park <parkdongho28@naver.com>
2023-04-07 19:51:42 +0900

    Update coursier-rhel-8-latest.sh

--- code/coursier/coursier-rhel-8-latest.sh
+++ code/coursier/coursier-rhel-8-latest.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+echo "==============================="
+echo "RUN : coursier-rhel-8-latest.sh"
+echo "==============================="
+
 curl -fL "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz" | gzip -d > cs
 chmod +x cs
 ./cs setup
 rm cs
+
+echo "================================"
+echo "EXIT : coursier-rhel-8-latest.sh"
+echo "================================"
