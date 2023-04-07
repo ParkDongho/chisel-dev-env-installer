d88bb38eeb5efc0865136bc49628bb9e7a840fa1
Dongho Park <parkdongho28@naver.com>
2023-04-07 20:04:33 +0900

    Update nvim-rhel-8-latest.sh

--- code/nvim/nvim-rhel-8-latest.sh
+++ code/nvim/nvim-rhel-8-latest.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+echo "==========================="
+echo "RUN : nvim-rhel-8-latest.sh"
+echo "==========================="
+
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo yum install -y neovim python3-neovim
 
+echo "============================"
+echo "EXIT : nvim-rhel-8-latest.sh"
+echo "============================"
