53c507b69aee3fbccb6818d3fc200f262d05240a
Dongho Park <parkdongho28@naver.com>
2023-04-07 13:15:27 +0900

    Update coursier-rhel-8-latest.sh

--- code/coursier/coursier-rhel-8-latest.sh
+++ code/coursier/coursier-rhel-8-latest.sh
@@ -1,4 +1,6 @@
+mkdir tmp && cd tmp && \
 sudo rm -f /etc/yum.repos.d/bintray-rpm.repo || true && \
 curl -L https://www.scala-sbt.org/sbt-rpm.repo > sbt-rpm.repo && \
 sudo mv sbt-rpm.repo /etc/yum.repos.d/ && \
-sudo yum install sbt
+sudo yum install sbt &&\
+cd .. && rm -rf tmp
