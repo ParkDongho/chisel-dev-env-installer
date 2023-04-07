15f94e042b7e9c5d2f1ddafb4f84b93fa6c365bd
Dongho Park <parkdongho28@naver.com>
2023-04-07 20:05:33 +0900

    Update zsh-rhel-8-latest.sh

--- code/ohmyzsh/zsh-rhel-8-latest.sh
+++ code/ohmyzsh/zsh-rhel-8-latest.sh
@@ -1,4 +1,13 @@
+#!/bin/sh
+echo "=========================="
+echo "RUN : zsh-rhel-8-latest.sh"
+echo "=========================="
+
 sudo yum -y install zsh
 sudo yum -y install util-linux-user
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s /bin/zsh
+
+echo "==========================="
+echo "EXIT : zsh-rhel-8-latest.sh"
+echo "==========================="
