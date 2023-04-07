e624e622ebed3b928e59d50460d94a29e448a6a4
Dongho Park <parkdongho28@naver.com>
2023-04-07 20:03:38 +0900

    Update java11-rhel-8-latest.sh

--- code/java/java11-rhel-8-latest.sh
+++ code/java/java11-rhel-8-latest.sh
@@ -1,2 +1,11 @@
+#!/bin/sh
+echo "============================="
+echo "RUN : java11-rhel-8-latest.sh"
+echo "============================="
+
 sudo yum -y install java-11-openjdk-devel.x86_64
 sudo update-alternatives --config java
+
+echo "=============================="
+echo "EXIT : java11-rhel-8-latest.sh"
+echo "=============================="
