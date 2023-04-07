efe0cf5293bc35f5be9524b8ffe9d3f7f2836d6e
Dongho Park <parkdongho28@naver.com>
2023-04-07 15:53:47 +0900

    Update sbt-rhel-8-latest.sh

--- code/sbt/sbt-rhel-8-latest.sh
+++ code/sbt/sbt-rhel-8-latest.sh
@@ -1,5 +1,11 @@
-sudo rm -f /etc/yum.repos.d/bintray-rpm.repo || true
-curl -L https://www.scala-sbt.org/sbt-rpm.repo > sbt-rpm.repo
-sudo mv sbt-rpm.repo /etc/yum.repos.d/
-sudo yum -y install sbt
+
+# Install sbt with cs setup
+cs setup
+sbt --script-version
+
+
+# sudo rm -f /etc/yum.repos.d/bintray-rpm.repo || true
+# curl -L https://www.scala-sbt.org/sbt-rpm.repo > sbt-rpm.repo
+# sudo mv sbt-rpm.repo /etc/yum.repos.d/
+# sudo yum -y install sbt
 
