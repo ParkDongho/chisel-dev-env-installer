db6a11cd10dc2512573f3209b21c16cdfbd233f4
Dongho Park <parkdongho28@naver.com>
2023-04-07 16:17:09 +0900

    Update conda-rhel-8-latest.sh

--- code/conda/conda-rhel-8-latest.sh
+++ code/conda/conda-rhel-8-latest.sh
@@ -2,6 +2,9 @@
 # Conda
 wget "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
 bash Mambaforge-$(uname)-$(uname -m).sh
+
+source ~/.zshrc
+
 conda config --set auto_activate_base false
 conda install -n base conda-lock
 conda activate base
