bd167bb0f2dd3433c6f760d52562197b3fa34772
Dongho Park <parkdongho28@naver.com>
2023-04-08 15:00:25 +0900

    Update conda-rhel-8-latest.sh

--- code/conda/conda-rhel-8-latest.sh
+++ code/conda/conda-rhel-8-latest.sh
@@ -4,6 +4,8 @@ echo "============================"
 echo "RUN : conda-rhel-8-latest.sh"
 echo "============================"
 
+sudo yum install wget -y
+
 # Conda
 # wget "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
 wget https://github.com/conda-forge/miniforge/releases/download/4.12.0-2/Mambaforge-Linux-x86_64.sh
