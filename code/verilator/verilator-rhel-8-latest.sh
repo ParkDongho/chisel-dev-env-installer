e469698a3c77db7f275fc042770fadd00370019c
Dongho Park <parkdongho28@naver.com>
2023-04-09 15:46:52 +0900

    Update verilator-rhel-8-latest.sh

--- code/verilator/verilator-rhel-8-latest.sh
+++ code/verilator/verilator-rhel-8-latest.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
+echo "================================"
+echo "RUN : verilator-rhel-8-latest.sh"
+echo "================================"
+
 sudo yum install -y https://kojipkgs.fedoraproject.org//packages/verilator/4.028/1.el8/x86_64/verilator-4.028-1.el8.x86_64.rpm
+
+echo "================================="
+echo "EXIT : verilator-rhel-8-latest.sh"
+echo "================================="
