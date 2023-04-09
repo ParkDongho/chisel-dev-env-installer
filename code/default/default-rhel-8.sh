25c3dc4b45f4841ea37c22a61a69bbc8f7cca7da
Dongho Park <parkdongho28@naver.com>
2023-04-09 15:42:00 +0900

    Update default-rhel-8.sh

--- code/default/default-rhel-8.sh
+++ code/default/default-rhel-8.sh
@@ -1,5 +1,13 @@
+echo "================================"
+echo "RUN : default-rhel-8.sh"
+echo "================================"
+
 sudo yum -y install net-tools vim make openssh-server \
   zsh firewalld gcc yum-utils openssl-devel bzip2-devel \
   libffi-devel zlib-devel ncurses-compat-libs sqlite-devel \
   git perl python3 make autoconf flex bison numactl wget
 sudo yum -y groupinstall "Development Tools"
+
+echo "================================"
+echo "EXIT : default-rhel-8.sh"
+echo "================================"
