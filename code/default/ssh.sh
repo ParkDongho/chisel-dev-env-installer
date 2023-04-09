3d21bc353f93cecc7b4bbfacd9df96306cc214a1
Dongho Park <parkdongho28@naver.com>
2023-04-09 15:42:24 +0900

    Update ssh.sh

--- code/default/ssh.sh
+++ code/default/ssh.sh
@@ -1,3 +1,11 @@
+echo "================================"
+echo "RUN : ssh.sh"
+echo "================================"
+
 sudo firewall-cmd --add-service=ssh --permanent
 sudo firewall-cmd --permanent --zone=public --add-port=22/tcp
 sudo firewall-cmd --reload
+
+echo "================================"
+echo "EXIT : ssh.sh"
+echo "================================"
