9cbbdd61f482b9b0a09fa10d5f1c05f66278b90a
Dongho Park <parkdongho28@naver.com>
2023-04-09 15:46:08 +0900

    Update rust-rhel-8-latest.sh

--- code/rust/rust-rhel-8-latest.sh
+++ code/rust/rust-rhel-8-latest.sh
@@ -1,2 +1,12 @@
+#!/bin/sh
+echo "==========================="
+echo "RUN : rust-rhel-8-latest.sh"
+echo "==========================="
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
+
+#!/bin/sh
+echo "============================"
+echo "EXIT : rust-rhel-8-latest.sh"
+echo "============================"
