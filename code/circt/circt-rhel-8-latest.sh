0ccd95e8274865cc84a17e7b420587bc5eb772f2
Dongho Park <parkdongho28@naver.com>
2023-04-12 22:45:55 +0900

    Create circt-rhel-8-latest.sh

---
+++ code/circt/circt-rhel-8-latest.sh
@@ -0,0 +1,33 @@
+git clone git@github.com:circt/circt.git ~/dev/circt
+cd ~/dev/circt
+git submodule init
+git submodule update
+
+cd llvm
+git fetch --unshallow
+
+#LLVM/MLIR 구축 및 테스트
+
+cd ~/dev/circt
+mkdir llvm/build
+cd llvm/build
+cmake -G Ninja ../llvm \
+    -DLLVM_ENABLE_PROJECTS="mlir" \
+    -DLLVM_TARGETS_TO_BUILD="X86;RISCV" \
+    -DLLVM_ENABLE_ASSERTIONS=ON \
+    -DCMAKE_BUILD_TYPE=DEBUG
+ninja
+ninja check-mlir
+
+# CIRCT 빌드 및 테스트:
+cd circt
+mkdir build
+cd build
+cmake -G Ninja .. \
+    -DMLIR_DIR=$PWD/../llvm/build/lib/cmake/mlir \
+    -DLLVM_DIR=$PWD/../llvm/build/lib/cmake/llvm \
+    -DLLVM_ENABLE_ASSERTIONS=ON \
+    -DCMAKE_BUILD_TYPE=DEBUG
+ninja
+ninja check-circt
+ninja check-circt-integration
