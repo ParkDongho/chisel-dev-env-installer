7352196c704f5f797121f3bb1b7caba0ef3e3c41
Dongho Park <parkdongho28@naver.com>
2023-04-21 12:42:35 +0900

    Update rust-ubuntu-20-latest.sh

--- code/rust/rust-ubuntu-20-latest.sh
+++ code/rust/rust-ubuntu-20-latest.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+source ../path/bashrc-ubuntu-20.sh
+
 echo "==========================="
 echo "RUN : rust-ubuntu-20-latest.sh"
 echo "==========================="
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source "$HOME/.cargo/env"
 
 #!/bin/sh
 echo "============================"
