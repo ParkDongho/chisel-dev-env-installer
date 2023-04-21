e49b5c97f74497181f67bbf61943defa2f194782
Dongho Park <parkdongho28@naver.com>
2023-04-21 12:40:46 +0900

    Update conda-setup-ubuntu-20-latest.sh

--- code/conda/conda-setup-ubuntu-20-latest.sh
+++ code/conda/conda-setup-ubuntu-20-latest.sh
@@ -1,26 +1,5 @@
 #!/bin/sh
-
-export PATH="$PATH:$HOME/.cargo/bin"
-export PATH="$PATH:$HOME/.local/share/coursier/bin"
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('$HOME/mambaforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-   eval "$__conda_setup"
-else
-   if [ -f "$HOME/mambaforge/etc/profile.d/conda.sh" ]; then
-       . "$HOME/mambaforge/etc/profile.d/conda.sh"
-   else
-       export PATH="$HOME/mambaforge/bin:$PATH"
-   fi
-fi
-unset __conda_setup
-
-if [ -f "$HOME/mambaforge/etc/profile.d/mamba.sh" ]; then
-   . "$HOME/mambaforge/etc/profile.d/mamba.sh"
-fi
-# <<< conda initialize <<<
+source ../path/bashrc-ubuntu-20.sh
 
 echo "=================================="
 echo "RUN : conda-setup-ubuntu-20-latest.sh"
