7e2f2b7320604f0359235cc4575edc65dff0fbae
Dongho Park <parkdongho28@naver.com>
2023-04-21 12:25:01 +0900

    Update conda-setup-ubuntu-20-latest.sh

--- code/conda/conda-setup-ubuntu-20-latest.sh
+++ code/conda/conda-setup-ubuntu-20-latest.sh
@@ -6,7 +6,7 @@ echo "=================================="
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('$HOME/mambaforge/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('$HOME/mambaforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
    eval "$__conda_setup"
 else
