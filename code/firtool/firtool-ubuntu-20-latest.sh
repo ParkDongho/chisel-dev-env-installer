a780b19e2677a0b31bf6a09681577c7b51d4e4b4
Dongho Park <parkdongho28@naver.com>
2023-04-23 18:44:23 +0900

    Update firtool-ubuntu-20-latest.sh

--- code/firtool/firtool-ubuntu-20-latest.sh
+++ code/firtool/firtool-ubuntu-20-latest.sh
@@ -1,3 +1,4 @@
-wget https://github.com/llvm/circt/releases/download/firtool-1.39.0/firrtl-bin-ubuntu-20.04.tar.gz ~/Downloads/firrtl-bin-ubuntu-20.04.tar.gz
+wget https://github.com/llvm/circt/releases/download/firtool-1.39.0/firrtl-bin-ubuntu-20.04.tar.gz
 tar -zxvf firrtl-bin-ubuntu-20.04.tar.gz
-sudo cp ~/Downloads/firtool-1.39.0/bin/firtool /usr/bin/firtool
+sudo cp ./firtool-1.39.0/bin/firtool /usr/bin/firtool
+rm -rf firrtl-bin-ubuntu-20.04.tar.gz firtool-1.39.0
