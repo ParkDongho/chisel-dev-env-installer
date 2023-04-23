7f8ee57d417e98c69c025b9fc33f70f97e07bbfe
Dongho Park <parkdongho28@naver.com>
2023-04-23 18:36:29 +0900

    Update diagrammer-ubuntu-20-latest.sh

--- code/diagrammer/diagrammer-ubuntu-20-latest.sh
+++ code/diagrammer/diagrammer-ubuntu-20-latest.sh
@@ -4,8 +4,9 @@ echo "RUN : diagrammer-ubuntu-20-latest.sh"
 echo "================================="
 
 sudo apt-get -y install graphviz
-rm -rf ~/.diagrammer
-git clone https://github.com/freechipsproject/diagrammer ~/.diagrammer
+mkdir ~/tools/
+rm -rf ~/tools/diagrammer
+git clone https://github.com/freechipsproject/diagrammer ~/tools/diagrammer
 
 echo "==================================="
 echo "EXIT : diagrammer-ubuntu-20-latest.sh"
