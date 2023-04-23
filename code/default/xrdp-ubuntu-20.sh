a8d39878e4ede04dfe1f8afda2fb229e601b6c3d
Dongho Park <parkdongho28@naver.com>
2023-04-23 17:57:44 +0900

    Update xrdp-ubuntu-20.sh

--- code/default/xrdp-ubuntu-20.sh
+++ code/default/xrdp-ubuntu-20.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+echo "================================"
+echo "RUN : xrdp-ubuntu-20.sh"
+echo "================================"
+
 sudo apt install xrdp
 sudo systemctl status xrdp
 sudo adduser xrdp ssl-cert
+
+echo "================================"
+echo "EXIT : xrdp-ubuntu-20.sh"
+echo "================================"
